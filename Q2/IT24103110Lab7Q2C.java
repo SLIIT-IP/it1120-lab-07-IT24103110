b1e72ad960426de73cbd90305ef6ae34e5e62b26
IT24103110 <it24103110@my.sliit.lk>
2024-09-23 22:49:20 +0530

    Add files via upload

---
+++ Q2/IT24103110Lab7Q2C.java
@@ -0,0 +1,15 @@
+public class IT24103110Lab7Q2C
+{
+	public static void main(String[] args) 
+	{
+		int x, y;
+		for(x = 5; x > 0; x=x-1)
+		{
+			for(y = 1; y <= x; y=y+1)
+			{
+				System.out.print(x+" ");
+			}
+			System.out.println(" ");
+		}
+	}
+}
